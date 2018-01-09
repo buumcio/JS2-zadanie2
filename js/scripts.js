83539e5753d3ea4a1bd096e156156d7388be9bd3
Magdalena Szymczak <mwozniak.kontakt@gmail.com>
2018-01-09 13:09:11 +0100

    Change function alert

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,8 @@ if (allNames.indexOf(newName) === -1) {
 	//allNames[allNames.length + 1] = neWName;
 	allNames.push(newName);
 	console.log(allNames);
+	alert("Super! Wprowadziłeś imię, którego jeszcze nie było!");
 } else {
-	console.log("Wprowadź inne imię");
+	console.log("Wprowadzone imię już istnieje!");
+	alert("Wprowadzone imię już jest w spisie imion. Wprowadź inne imię!");
 }
